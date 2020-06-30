75c72592a10725dcd7fe9a09f222cfa8b538b34d
Arturo-AB <arturo.amoroso@atombeamtech.com>
2020-06-30 18:25:57 -0400

    Second test

--- Unittest.py
+++ Unittest.py
@@ -5,5 +5,8 @@ class TestCalculator(unittest.TestCase):
 	def test_cinco_mas_cinco_igual_diez(self):
 		assert 5 + 5 == 10
 
+	def test_cinco_por_cinco(self):
+		assert 5 * 5 == 25
+
 if __name__ == "__main__":
 	unittest.main()
